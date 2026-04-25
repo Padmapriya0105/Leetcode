36223e8dbec0bc424503d65149cc8eb124754061
Padmapriya0105 <pp179174_mca27@mepcoeng.ac.in>
2026-04-25 21:26:30 +0530

    Create Minimum Window Substring.java

---
+++ Minimum Window Substring.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) map[c]++;
+
+        int count = t.length();
+        int left = 0, right = 0, minLen = Integer.MAX_VALUE, start = 0;
+
+        while (right < s.length()) {
+            if (map[s.charAt(right++)]-- > 0) count--;
+
+            while (count == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+                if (map[s.charAt(left++)]++ == 0) count++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
