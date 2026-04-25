f3fdbcbbfbdd0c95e7afca756fb855334ccae565
Padmapriya0105 <pp179174_mca27@mepcoeng.ac.in>
2026-04-25 21:59:55 +0530

    Create  Rotate List.js

---
+++ Rotate List.js
@@ -0,0 +1,25 @@
+var rotateRight = function(head, k) {
+    if (!head || !head.next || k === 0) return head;
+
+    let length = 1;
+    let tail = head;
+    while (tail.next) {
+        tail = tail.next;
+        length++;
+    }
+
+    k = k % length;
+    if (k === 0) return head;
+
+    let stepsToNewTail = length - k;
+    let newTail = head;
+    for (let i = 1; i < stepsToNewTail; i++) {
+        newTail = newTail.next;
+    }
+
+    let newHead = newTail.next;
+    newTail.next = null;
+    tail.next = head;
+
+    return newHead;
+};
