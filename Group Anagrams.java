3197b950d1dde37860448be407bde1f10e1433bf
Padmapriya0105 <pp179174_mca27@mepcoeng.ac.in>
2026-04-25 10:41:29 +0530

    Create Group Anagrams.java

---
+++ Group Anagrams.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] characters = s.toCharArray();
+            Arrays.sort(characters);
+            String sorted = new String(characters);
+            
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
